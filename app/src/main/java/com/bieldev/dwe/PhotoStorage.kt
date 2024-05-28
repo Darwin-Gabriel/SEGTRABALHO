5793d265d2595f651266119ffe51bb6911b95a69
Darwin-Gabriel <gabriel.figueiredo@darwineng.com.br>
2024-05-28 14:44:01 -0300

    Commit inicial 3

---
+++ app/src/main/java/com/bieldev/dwe/PhotoStorage.kt
@@ -0,0 +1,10 @@
+package com.bieldev.dwe
+
+import android.net.Uri
+
+object PhotoStorage {
+    var photoStep1: Uri? = null
+    var photoStep2: Uri? = null
+    var photoStep3: Uri? = null
+    var photoStep4: Uri? = null
+}
